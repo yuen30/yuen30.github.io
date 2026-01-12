756f88eb9cdc84a5a56372a38b40413d65abf2cb
yuen30 <taweechai.yue@yahoo.com>
2026-01-12 15:57:49 +0700

    test deploy up

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+import createMDX from '@next/mdx'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+    experimental: {
+        mdxRs: true,
+    },
+}
+
+const withMDX = createMDX({
+    extension: /\.mdx?$/,
+    options: {
+        remarkPlugins: [],
+        rehypePlugins: [],
+    },
+})
+
+export default withMDX(nextConfig)
