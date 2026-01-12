c35352eacbf03218607d66f4be442aff5ef22506
yuen30 <taweechai.yue@yahoo.com>
2026-01-12 15:54:04 +0700

    test deploy with bun

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 const withMDX = createMDX({
